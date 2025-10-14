ed0af16901b63cd6dd79ac29ba59489488f563ed
fdanctl <daniel.a.d.freire.00@gmail.com>
2025-10-14 18:44:55 +0100

    style: horizontal-scroll border fade

--- js/horizontal-scroll.js
+++ js/horizontal-scroll.js
@@ -1,4 +1,6 @@
 const slider = document.querySelector(".horizontal-scroll");
+const borderL = document.querySelector(".border-fade.fade-left");
+const borderR = document.querySelector(".border-fade.fade-right");
 
 let isDragging = false;
 let startX;
@@ -25,3 +27,21 @@ slider.addEventListener("mousemove", (e) => {
   const walk = x - startX;
   slider.scrollLeft = scrollLeft - walk;
 });
+
+(borderL || borderR) &&
+  slider.addEventListener("scroll", (e) => {
+    const scrollLeft = e.target.scrollLeft;
+    const maxScrollLeft = e.target.scrollWidth - e.target.clientWidth;
+
+    if (scrollLeft > 16) {
+      borderL.classList.add("visible");
+    } else {
+      borderL.classList.remove("visible");
+    }
+
+    if (scrollLeft < maxScrollLeft - 16) {
+      borderR.classList.add("visible");
+    } else {
+      borderR.classList.remove("visible");
+    }
+  });
